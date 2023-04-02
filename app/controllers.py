2ae6b61d005d8b044c8ffc6dc82b0836ba94e865
sSpicey <martini@alunos.utfpr.edu.br>
2023-04-02 16:25:24 -0300

    Split 3rd endpoint into smaller functions

--- app/controllers.py
+++ app/controllers.py
@@ -12,11 +12,20 @@ def average_words_length(phrase: str) -> float:
     total_letters = sum(len(word) for word in words)
     return total_letters / len(words) if words else 0
 
-
 def matched_and_mismatched_words(phrase1: str, phrase2: str) -> tuple[list[str], list[str]]:
+    set1, set2 = get_word_sets(phrase1, phrase2)
+    mismatched_words = get_symmetric_difference(set1, set2)
+    matched_words = get_intersection(set1, set2)
+    return mismatched_words, matched_words
+
+def get_word_sets(phrase1: str, phrase2: str) -> tuple[set[str], set[str]]:
     set1 = set(phrase1.split())
     set2 = set(phrase2.split())
-    mismatched_words = sorted(list(set1.symmetric_difference(set2)))
-    matched_words = sorted(list(set1.intersection(set2)))
+    return set1, set2
 
-    return mismatched_words, matched_words
+def get_symmetric_difference(set1: set[str], set2: set[str]) -> list[str]:
+    return sorted(list(set1.symmetric_difference(set2)))
+
+def get_intersection(set1: set[str], set2: set[str]) -> list[str]:
+    return sorted(list(set1.intersection(set2)))
+
