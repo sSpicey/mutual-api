64d3dc50b95ef025e0739eace229aff656a745f9
sSpicey <martini@alunos.utfpr.edu.br>
2023-04-02 16:17:45 -0300

    Add edge case protection to business logic

--- app/controllers.py
+++ app/controllers.py
@@ -1,16 +1,22 @@
+import re
+
 def reverse_integer(number: int) -> int:
     is_negative = number < 0
     reverse_abs = int(str(abs(number))[::-1])
+
     return -reverse_abs if is_negative else reverse_abs
 
 def average_words_length(phrase: str) -> float:
-    words = phrase.split()
+    words = re.findall(r'\w+', phrase)
+    print(words)
     total_letters = sum(len(word) for word in words)
-    return total_letters / len(words)
+    return total_letters / len(words) if words else 0
+
 
 def matched_and_mismatched_words(phrase1: str, phrase2: str) -> tuple[list[str], list[str]]:
     set1 = set(phrase1.split())
     set2 = set(phrase2.split())
-    uncommon_words = sorted(list(set1.symmetric_difference(set2)))
-    common_words = sorted(list(set1.intersection(set2)))
-    return uncommon_words, common_words
+    mismatched_words = sorted(list(set1.symmetric_difference(set2)))
+    matched_words = sorted(list(set1.intersection(set2)))
+
+    return mismatched_words, matched_words
