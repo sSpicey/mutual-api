f09135c64a085dccf1bf71e8214d97cd27bd7dbf
sSpicey <martini@alunos.utfpr.edu.br>
2023-04-02 15:34:28 -0300

    Refactor view variable names

--- app/views.py
+++ app/views.py
@@ -8,14 +8,14 @@ matched_and_mismatched_words_router = APIRouter()
 
 @reverse_integer_router.get("/reverse_integer/{number}")
 def get_reverse_integer(number: int):
-    return {"reverse_integer": reverse_integer(number)}
+    return {"reverse integer": reverse_integer(number)}
 
-@average_words_length_router.post("/medium_length")
+@average_words_length_router.post("/average_length")
 def post_average_words_length(request: AverageWordsLengthRequest):
-    return {"medium_length": average_words_length(request.phrase)}
+    return {"average length": average_words_length(request.phrase)}
 
-@matched_and_mismatched_words_router.post("/common_words")
+@matched_and_mismatched_words_router.post("/matched_mismatched_words")
 def post_matched_and_mismatched_words(request: MatchedAndMismatchedWordsRequest):
     phrase1, phrase2 = request.phrase1, request.phrase2
-    uncommon_words, common_words = matched_and_mismatched_words(phrase1, phrase2)
-    return {"uncommon_words": uncommon_words, "common_words": common_words}
+    matched_words, mismatched_words = matched_and_mismatched_words(phrase1, phrase2)
+    return {"matched words": matched_words, "mismatched words": mismatched_words}
